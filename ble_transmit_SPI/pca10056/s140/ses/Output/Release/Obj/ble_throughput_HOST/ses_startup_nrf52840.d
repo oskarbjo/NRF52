8bbafe9e7ead5aa01aacb9d4de9e99dc324b26ac
Oskar Bjoerkqvist <objoerkqvist@eeibt032.d.ethz.ch>
2021-12-06 10:20:47 +0100

    First commit

---
+++ ble_transmit_SPI/pca10056/s140/ses/Output/Release/Obj/ble_throughput_HOST/ses_startup_nrf52840.d
@@ -0,0 +1,2 @@
+Output/Release/Obj/ble_throughput_HOST/ses_startup_nrf52840.o: \
+ C\:\Users\objoerkqvist\Segger\nRF5_SDK_17.1.0_ddde560\modules\nrfx\mdk\ses_startup_nrf52840.s
---
+++ ble_transmit_SPI/pca10056/s140/ses/Output/Release/Obj/ble_throughput_HOST/thumb_crt0.d
@@ -0,0 +1,2 @@
+Output/Release/Obj/ble_throughput_HOST/thumb_crt0.o: \
+ C\:/Program\ Files/Segger/arm_segger_embedded_studio_v560_win_x64_nordic/source/thumb_crt0.s
